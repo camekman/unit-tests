7b269324bb0bc99eb826cb47fee6ffa189d0e0b5
camekman <ekman.camilla@gmail.com>
2021-10-01 11:08:32 +0200

    completed task 1-6

--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -1,9 +1,13 @@
 export const isEmpty = (stringArrayOrObject) => {
-  const type = typeof stringArrayOrObject
+  const type = typeof stringArrayOrObject;
 
-  if (type === 'string') {
-    return stringArrayOrObject === ''
+  if (type === "string") {
+    return stringArrayOrObject === "";
+  } else if (type === "object" && stringArrayOrObject.length === 0) {
+    return true;
+  } else {
+    return false;
   }
+};
 
-  return false
-}
+//method object keys ...
